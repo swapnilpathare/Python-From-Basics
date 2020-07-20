2c23e9aff1908e26935d2c5043e1c8e7e66ff504
Swapnil Pathare <swapnilpathare123@gmail.com>
2020-07-20 23:42:35 +0530

    Update loops-and-conditions.py

--- loops-and-conditions.py
+++ loops-and-conditions.py
@@ -21,6 +21,15 @@ for i in (0, 1, 2, 3, 4, 5):
 
 
 # How to use 'While' Loop in python to print statement 3 times
+# example 1
+i = 1                                 #defining the variable
+while i < 6:                          #while loop with condition
+  print(i)                            #print statement
+  i += 1                              #i++ to increment value by 1
+# outout- 1
+#         2
+
+# example 2
 c=2                                   #define the variable
 while c==2:                           #declaring while loop with condition
     if c == 2:                        #condition to be check
