3cd433dc1937d5ce6fdcf7e6ef290c89932f0d0f
Swapnil Pathare <swapnilpathare123@gmail.com>
2020-07-20 23:28:43 +0530

    Create print-and-loops.py

---
+++ print-and-loops.py
@@ -0,0 +1,12 @@
+# To print any word/statement in python
+print('Hello, World...!')
+# output-Hello, World...!
+
+# How to use 'For' Loop in python to print statement 3 times
+for i in range ( 3 ):
+  print ( "Hello, World...!" )
+# output- Hello, World...!                    
+#         Hello, World...!
+#         Hello, World...!
+
+
